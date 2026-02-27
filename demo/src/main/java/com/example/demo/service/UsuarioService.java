25275b6b1f64db7f0002c151708527ad08b2f64e
Mateus Sena <mateussantos192007@gmail.com>
2026-02-27 00:27:35 -0300

    feat: add Security token

--- demo/src/main/java/com/example/demo/service/UsuarioService.java
+++ demo/src/main/java/com/example/demo/service/UsuarioService.java
@@ -1,7 +1,31 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Usuario;
 import com.example.demo.repository.UsuarioRepository;
 
+@Service
 public class UsuarioService {
-    
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    public Usuario registrarUsuario(String username, String password) {
+        String senhaCriptografada = passwordEncoder.encode(password);
+        Usuario usuario = new Usuario(username, senhaCriptografada);
+        return usuarioRepository.save(usuario);
+    }
+
+    public Optional<Usuario> buscarPorUsername(String username) {
+        return usuarioRepository.findByUsername(username);
+    }
 }
